d74e22a2c833d75cb9e81d9d4df7d1d267e8a3b5
kcn3388 <kcn3388@126.com>
2022-01-06 10:53:13 +0800

    fix base path for personal use

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	port: '80',
     title: 'xcw帮助',
     description: 'xcw帮助',
-    base: '/',
+    base: '/docs/xcw/',
     plugins: {
         '@vuepress/search': {
             searchMaxSuggestions: 10
